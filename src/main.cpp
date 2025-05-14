096490ae76fa2406610a35a0058f3f8ad07667ff
Ahmet Yunus BAYRAM <84668188+AhmetYunusBAYRAM@users.noreply.github.com>
2025-05-14 23:48:26 +0300

    son son son

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include "Arena.cpp"  // Arena sınıfı tanımı
-#include "../include/config.h"  // Konfigürasyon sabitleri (ARENA_WIDTH, ARENA_HEIGHT)
+#include <iostream>
 #include <cstdlib>  // srand ve rand fonksiyonları için
 #include <ctime>    // time fonksiyonu için
 
 int main() {
-    // Rastgele sayı üreticisinin başlatılması
-    srand(time(0));
-
-    // Arena nesnesi oluşturuluyor ve boyutlar parametre olarak veriliyor
-    Arena arena(ARENA_WIDTH, ARENA_HEIGHT);
-
-    // Arena başlatılıyor, robotlar ekleniyor vs.
+    // Rastgele sayı üretecini başlat
+    srand(time(nullptr));
+    
+    // Oyun alanı boyutlarını belirle
+    const int WIDTH = 20;
+    const int HEIGHT = 10;
+    
+    // Oyun alanını oluştur
+    Arena arena(WIDTH, HEIGHT);
+    
+    // Oyunu başlat
+    std::cout << "RobotWars'a Hoş Geldiniz!\n\n";
     arena.initialize();
-
-    // Oyun başlatılıyor
     arena.runGame();
-
+    
     return 0;
 }
